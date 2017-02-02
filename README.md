57cd2103032d75805c385f4d56dc4c9398f71227
Jorre <jorre@outlook.com>
2017-02-02 11:45:50 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gulp-wp
+![macOS](https://img.shields.io/badge/OS-macOS-blue.svg)
+![Wordpress](https://img.shields.io/wordpress/v/akismet.svg)
+![Gulp](https://img.shields.io/badge/Gulp-3.9.1-red.svg)
+![Local](https://img.shields.io/badge/Local-1.3.0-brightgreen.svg)
+
+Theme from: Underscores.me
+
+## Installation
+
+
+## Use
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+/// VARIABLES ///
+
+var gulp 						= require('gulp');
+var sass 						= require('gulp-sass');
+var clean 					= require('gulp-clean');
+var shell						= require('gulp-shell');
+var plumber 				= require('gulp-plumber');
+var runSequence     = require('run-sequence').use(gulp);
+var browserSync 		= require('browser-sync').create();
+
+/// Config
+
+var theme_name 			= "test";  // Needs to be the same as the 'Local' theme name
+var project_name 		=	"test"
+var location_wp 		= "local-sites/" + project_name + "/app/public/wp-content/themes/" + theme_name; // Make sure the folder where the 'Local' websites are stored is the right one.
+var location_build 	= "build";
+
+/// TASKS ///
+
+gulp.task('default', function(){
+	console.log(" ");
+	console.log("  Theme: " + theme_name );
+	console.log(" ");
+	console.log("  gulp build  │  Compile all files to /build/");
+	console.log("  gulp sass   │  Compile sass files to /build/");
+	console.log("  gulp php    │  Compile php files to /build/");
+	console.log("  gulp clean  │  Clean build directory");
+	console.log("  gulp deploy │  Deploy to local wordpress server");
+	console.log("  gulp watch  │  Watch changes, build and deploy automatically");
+	console.log(" ");
+});
+
+gulp.task('build', function(){
+	return gulp.src(['src/**/*'])
+		.pipe(plumber())
+		.pipe(gulp.dest(location_build));
+});
+
+gulp.task('clean', function(){
+	return gulp.src(location_build)
+		.pipe(plumber())
+		.pipe(clean());
+})
+
+gulp.task('sass', function(){
+	return gulp.src('src/sass/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest(location_build))
+		.pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('php', function(){
+	return gulp.src('src/**/*.php')
+		.pipe(plumber())
+		.pipe(gulp.dest(location_build))
+		.pipe(browserSync.reload({
+            stream: true
+        }));
+})
+
+
+gulp.task('deploy', function(){
+	return gulp.src('build/**/*')
+		.pipe(shell('cp -R '+ location_build +'/. ~/' + location_wp));
+});
+
+
+gulp.task('watch', function() {
+    browserSync.init({
+        proxy: theme_name + ".dev"
+    });
+
+    gulp.watch('src/**/*.php', () => runSequence(['php'], ['deploy'])).on("change", browserSync.reload)
+  	gulp.watch('src/sass/**/*.scss', () => runSequence(['sass'], ['deploy'])).on("change", browserSync.reload)
+});
